2f973f3a6b2a00f7a7d19f546078a53aa3cf6d2b
starup <pe7913@hotmail.com>
2020-04-02 14:15:31 +0200

    Changes

--- AjourWebsite/accounts/urls.py
+++ AjourWebsite/accounts/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.indexView, name='home'),
     path('dashboard/', views.dashboardView, name='dashboard'),
-    path('login/', LoginView.as_view(), name='login_url'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login_url'),
     path('logout/', LogoutView.as_view(next_page='dashboard'), name='logout'),
     path('register/', views.registerView, name='register_url')
 ]
